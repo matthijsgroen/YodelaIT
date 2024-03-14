cc5cefbad2fb2c085baf0ff9d84fac5c8fe5ddba
Matthijs Groen <matthijs.groen@gmail.com>
2024-03-14 11:13:19 +0100

    Prepare for deployment

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://matthijsgroen.github.io/",
+  url: "https://levenslied.kabisa.nl/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/YodelaIT/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

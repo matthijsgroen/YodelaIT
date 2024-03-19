c8120f2ad1ebdd9ab9b07e356eaee176c878ffe8
Matthijs Groen <matthijs.groen@gmail.com>
2024-03-19 15:39:12 +0100

    Add analytics

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,6 +16,9 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
+  scripts: [
+     { src: 'https://plausible.io/js/script.js', defer: true, 'data-domain': 'levenslied.kabisa.nl'},
+  ]
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
